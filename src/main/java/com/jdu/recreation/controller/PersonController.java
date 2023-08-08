f6c434ca8bdf5a389badaf51d990f9e358b65eb1
Daun Jeong <prixia@naver.com>
2023-08-08 18:51:43 +0900

    Proverb - 검색 조건 쿼리 추가 / Person - param 어노테이션 수정

--- src/main/java/com/jdu/recreation/controller/PersonController.java
+++ src/main/java/com/jdu/recreation/controller/PersonController.java
@@ -4,10 +4,7 @@ import com.jdu.recreation.model.vo.PersonVo;
 import com.jdu.recreation.service.interfaces.PersonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +14,7 @@ public class PersonController {
     private final PersonService personService;
 
     @PostMapping("/get")
-    public ResponseEntity<Object> get(@RequestParam PersonVo vo) {
+    public ResponseEntity<Object> get(@RequestBody PersonVo vo) {
         return ResponseEntity.ok().body(personService.getRandomPerson(vo));
     }
 }
